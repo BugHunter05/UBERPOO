434807c9d6be11c2149879e64857987fadc0fc96
Carlos <vasquezrafaelcarlosadalbert@gmail.com>
2024-04-05 12:11:17 -0600

    arhivos de codigo

---
+++ Java/User.java
@@ -0,0 +1,17 @@
+class User extends Account{
+  public User (String name, string
+        document, String email, string
+        password){
+ 
+           super(name, document, email,
+           password);
+        }
+        void printDataUser(){
+           system.out.printn("Document
+            driver: " + document + "Name 
+            driver: " + name + "E-mail: "
+            + email + "Password: "+
+            password);
+        } 
+}
+
